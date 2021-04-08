ddc974bdd94e22e21f6e8f497253804b0bd69881
julypenguin <kaopenguin@gmail.com>
2021-04-08 23:04:59 +0800

    基本環境建立

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        "144": "36rem",
+        "176": "44rem",
+      },
+      padding: {
+        "144": "36rem",
+        "176": "44rem",
+      }
+    },
+    // container: {
+    //   center: true,
+    // },
+  },
+  variants: {
+    extend: {},
+  }
+  // plugins: [
+  //   require('@tailwindcss/forms'),
+  // ],
+}
